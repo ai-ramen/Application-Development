28796f444bd0356aed263df0f43231d2fef5cfe0
ai-ramen <19102860@usc.edu.ph>
2021-10-05 14:56:34 +0800

    a

---
+++ To do list/demo-project/src/App.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+import "./App.css";
+
+function App() {
+  const[count,setcount]=useState(0);
+
+  return (
+    <div className="App">
+      <h1>this is a react app</h1>
+    </div>
+  )
+}
+
+export default App;
